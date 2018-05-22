266dca22eeb3788a379a211ea20f498c4e9e27e2
João Pedro Costa <joaopcosta@gmail.com>
2018-05-22 13:27:34 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var then = new Date(2018, 05, 02), // month is zero based
     now  = new Date;               // no arguments -> current date
 var element = document.getElementsByClassName('my-counter');
 // 24 hours, 60 minutes, 60 seconds, 1000 milliseconds
-var value = Math.round((then - now) / (1000 * 60 * 60 * 24)); // round the amount of days
+var value = Math.round((then - now) / (1000 * 60 * 60 * 24)) + 1; // round the amount of days
 // result: 712
 var el1 = element[0];
 var el2 = element[1];
